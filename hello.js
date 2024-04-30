1d386d748bedd6a38d045f62c0d35b01c1d6fb8e
Sahil <sahilbdabhi@gmail.com>
2024-04-30 20:06:58 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+// to write anthing we use console.log and whatever we need to display in the console of the browser 
+console.log("hello")
+
+
+var message = "hi there"
+var message = "hello"
+message = "hello again"
+console.log(message)
+
+// var can be used again and again whenever we want to make a variable it will replace the value of the variable to the latest update whenever it is called.

e5fbe205ceba5169fa20b0b10beea8f872250b8e
Sahil <sahilbdabhi@gmail.com>
2024-04-30 21:28:32 +0530

    datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,38 @@
+// the types of data and how to use each and every data type
+
+// to check the type of variable , we van use console.log(typeof variableName)
+
+// string - it is a sequence of characters
+let userName = 'Sahil'
+console.log(userName)
+
+// number
+
+let age =  21
+
+// boolean only true or false
+
+let isProductInCart = false
+console.log(isProductInCart)
+
+// undefined -- we only define variable not its value
+let value 
+console.log(typeof value)
+
+
+// null
+
+let noValue = null
+console.log(noValue)
+
+
+// Object
+
+const myDetails = {name: "Sahil" , age: 21 , Degree: "Engineering"}
+console.log(myDetails)
+
+// array
+
+const arrayOfNumber = [1,2,3,4]
+console.log(arrayOfNumber)
+console.log(typeof arrayOfNumber)
